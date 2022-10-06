1a78331585231efec21b6c06240d677d41d95b34
Samuel Adler <masko552@gmail.com>
2022-10-06 21:59:16 +0200

    firstCommit
    - first setup

---
+++ QuizAppDiploma/app/src/main/java/com/example/quizappdiploma/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.quizappdiploma
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+
+        val navController = navHostFragment.navController
+        findViewById<BottomNavigationView>(R.id.nav_view)
+            .setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean
+    {
+        return Navigation.findNavController(this, R.id.nav_host_fragment).navigateUp()
+    }
+}
---
+++ QuizAppDiploma/app/src/main/java/com/example/quizappdiploma/fragments/WelcomeFragment.kt
@@ -0,0 +1,30 @@
+package com.example.quizappdiploma.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import com.airbnb.lottie.LottieAnimationView
+import com.example.quizappdiploma.R
+
+class WelcomeFragment : Fragment()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        parentFragment?.activity?.actionBar?.hide()
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View?
+    {
+        return inflater.inflate(R.layout.welcome_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?)
+    {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
